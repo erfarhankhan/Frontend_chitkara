87bd53779563302dbb750a81ec5f7b455aa73985
erfarhankhan <hfkhan991@gmail.com>
2024-02-15 22:42:19 +0530

    jsloop

---
+++ G8/lec27/script/script.js
@@ -0,0 +1,31 @@
+// alert("hello from script js"); 
+
+
+// let a =10;
+// let b = 20;
+// if(a<b){
+//     console.log("a is smaller ");
+// }
+// if(b>a){
+//     console.log("b is greater ");
+// }
+
+// const num =100;
+// if(num% 2 == 0){
+//     console.log("EVEN!!"); 
+// }else{
+//     console.log("odd!!")
+// }
+
+// const age = parseInt(prompt("Enter your age "));
+// console.log("age");
+// console.log(typeof age);
+
+// const age = parseInt(prompt("Enter your age "));
+// if(age<18){
+//     console.log("u cant enter");
+// }else if(age>=18 && age<=25){
+//     console.log("you can enter but cant drink");
+// }else {
+//     console.log("u can enter and drink coffee");
+// }
