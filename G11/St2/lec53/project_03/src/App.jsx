7d02a2680e046ece4bbf1bccb23046c7d04b92fc
hf <hfkhan991@gmail.com>
2024-03-27 16:11:46 +0530

    babel

---
+++ G11/St2/lec53/project_03/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Babel from './Babel'
+
+const App = () => {
+  return (
+    <div>
+      <h1>hello </h1>
+   <Babel />
+    </div>
+  )
+}
+
+export default App
---
+++ G8/St2/lec53/project_03/src/Babel.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+const Babel = () => {
+  return _jsxs("div", {
+    children: [/*#__PURE__*/_jsx("h1", {
+      children: "hello ract js from babel jsx with babel syntax"
+    }), _jsx("h2", {
+      children: "Babel"
+    }), _jsx("p", {
+      children: "Lorem ipsum dolo earum!"
+    })]
+  });
+};
+export default Babel;
